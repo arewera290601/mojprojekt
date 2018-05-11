306edbc2e6198de10a7b2416645d10f038123f23
arewera290601 <aleksandrarewera@gmail.com>
2018-05-11 13:30:35 +0200

    operqacje dadoawania i aktualizacji sqlite3

--- sql/szkola.sql
+++ sql/szkola.sql
@@ -21,3 +21,12 @@ CREATE TABLE tbKlasa
 		rokNaboru INTEGER,
 		rokMatury INTEGER
 );
+
+INSERT INTO tbklasa(id, klasa, rokNaboru, rokMatury) VALUES (NULL, '1A', 2017, 2020);
+INSERT INTO tbklasa VALUES (NULL, '2A', 2016, 2019);
+INSERT INTO tbklasa VALUES (NULL, '1C', 2017, 2020);
+INSERT INTO tbUczniowie(id, imie, nazwisko, plec, id_klasa, egzHum, egzMat, egzJez)
+VALUES(NULL, 'Adam', 'Słodki', 0, 3, 70.5, 80.4, 98.4);
+INSERT INTO tbUczniowie VALUES (NULL, 'Aleksandra', 'Rewera', 1, 1, 83, 75, 70);
+
+UPDATE tbUczniowie SET egzJez = 100 WHERE id = 1;
