49d660a085910b56c51d77dcc1b9be60bb0fb0a4
arewera290601 <aleksandrarewera@gmail.com>
2018-09-07 14:18:45 +0200

    cpp

---
+++ cpp/szablon.cpp
@@ -0,0 +1,18 @@
+/*
+ * szablon.cpp
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a; // deklaracja zmiennej
+    a = 0; // inicjacja zmiennej 
+    cout << "Podaj liczbę: ";
+    cin >> a; // zatrzyma wykonywanie probramu czeka aż użytkownik wprowadzi zmienna i kliknie enter
+    cout << a;  
+	return 0;
+}
+
