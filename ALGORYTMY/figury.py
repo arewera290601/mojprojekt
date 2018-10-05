e7a851e07d8aaa368925c8a5fbc927e3b2de75d7
arewera290601 <aleksandrarewera@gmail.com>
2018-10-05 14:19:41 +0200

    figury

--- ALGORYTMY/figury.py
+++ ALGORYTMY/figury.py
@@ -26,31 +26,54 @@ def prostokat2(a, b, c): #drukowanie pustego prostokąta
     c = str(input("Podaj znak prostokata: "))
     
     
-    for i in range(a):
-        for j in range(b):
-            if i 
-            if j == 0 or j == b - 1:
-                print(c, end='')
-            else:
-                print(" ", end='')
+    for i in range(a): # pętla zewnętrzna odpowiada za wiersze
+       for j in range(b): # pętla wewnętrzna
+           if j == 0 or j == b - 1 or i == 0 or i == a - 1:
+               print(c, end='')
+           else:
+               print(" ", end='')
+       print() # znak końca linii, tj. przejście do następnego wiersza
+    
+    return 0
+
+def choinka(h, c):
+    h = int(input("Podaj wysokość choinki: "))
+    c = str(input("Podaj znak z którego ma byc zbudowana choinka: "))
+    
+    for i in range(h):
+        for j in range(i + 1):
+            print(c, end= '')
         print()
     
     return 0
     
-def main(args):
-    a = int(input("Podaj szerokość prostokąta: "))
-    b = int(input("Podaj wysokość prostokata: "))
-    c = str(input("Podaj znak prostokata: "))
+def choinka2(h, c):
+    h = int(input("Podaj wysokość choinki: "))
+    c = str(input("Podaj znak z którego ma byc zbudowana choinka: "))
     
-    
-    for i in range(a):
-        for j in range(b):
-            if i 
-            if j == 0 or j == b - 1:
-                print(c, end='')
-            else:
-                print(" ", end='')
+    for i in range(h):
+        for j in range(h - i):
+            print(c, end= '')
         print()
+    
+    return 0
+
+
+def main(args):
+    a = int(input("Podaj długość podstawy trójkąta: "))
+    h = int(input("Podaj wysokość trojkąta: "))
+    c = str(input("Podaj znak z którego ma byc zbudowana trójkąt: "))
+    
+    for i in range(h):
+        for j in range(h + i):
+            print(c, end= '')
+        print()
+  
+    
+     
+            
+    
+    
     
     
     return 0
