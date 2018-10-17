be8ba7f120972f251036f41c700c5f7e350ad259
arewera290601 <aleksandrarewera@gmail.com>
2018-10-17 10:35:39 +0200

    c++

--- ALGORYTMY/tabliczka.py
+++ ALGORYTMY/tabliczka.py
@@ -3,20 +3,19 @@
 #
 #  tabliczka.py
 
-def tabliczka():
+def tabliczka(a, b):
+
     
-    i = 0
-    j = 0
-    
-    for i in range(1, 11):
-        for j in range(1, 11):
-            print("{:>3} ".format(i * j), end= '')
+    for i in range(1, a +1):
+        for j in range(1, b + 1):
+            print("{:>4} ".format(i * j), end= '')
         print()
     
 
 def main(args):
-    
-    tabliczka()
+    a = int(input("Podaj zakres 1: "))
+    b = int(input("Podaj zakres 2: "))
+    tabliczka(a, b)
     return 0
 
 if __name__ == '__main__':
