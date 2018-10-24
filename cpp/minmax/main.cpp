5deb25e01459c6554c20de92c7eb06639e1d1aad
arewera290601 <aleksandrarewera@gmail.com>
2018-10-24 10:34:32 +0200

    euklines.cpp

---
+++ cpp/minmax/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void minmax(){
+    char dalej = 't';
+    int liczba = 0;
+    int min = 0;
+    while(dalej = 't'){
+        cout << "Podaj liczbę: ";
+
+    }
+}
+
+int main()
+{
+    minmax();
+    return 0;
+}
