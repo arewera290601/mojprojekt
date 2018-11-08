e967e72e7333a90b3043f77491a0d55bc88d1f9c
arewera290601 <aleksandrarewera@gmail.com>
2018-11-08 08:46:08 +0100

    zamianaliter

--- cpp/licz_znaki.cpp
+++ cpp/licz_znaki.cpp
@@ -5,6 +5,8 @@
 
 
 #include <iostream>
+#include <string.h>
+
 
 using namespace std;
 
@@ -18,7 +20,7 @@ void zamiana_liter(char tab[])
             kod += 32;
         else if (kod >= 97 && kod <= 122)
             kod -= 32;
-        cout << (char)kod;  
+        cout << (char)kod << " ";
         i++;
     }
 }
@@ -54,14 +56,41 @@ void licz_znaki(char tb[], int roz) {
     cout << "Reszta: " << reszta << endl; 
 }
 
+int zlicz(char tb[]){
+    int i = 0;
+    while (tb[i] != '\0') i++;
+    return i;
+    }
+
+
+void ascii(char tb[], int roz){
+    for(int i = 0; i < roz; i++) {
+        kod = (int)tb[i];
+        cout << (int)tb[i] << " ";
+    }
+}
+
+
+void odwroc(char tb[], int roz) {
+    for(int i = roz-0; i >= 0; i--)
+        cout << tb[i] << " ";
+    
+    }
 
 int main(int argc, char **argv)
 {
     const int rozmiar = 20;
 	char znaki[rozmiar];
     cin.getline(znaki, rozmiar);
-    licz_znaki(znaki, cin.gcount());
-    zamiana_liter(znaki);
+    //licz_znaki(znaki, cin.gcount());
+    zamiana_liter(znaki); 
+    int ilosc = 0; 
+    // ilosc = cin.gcount();
+   // ilosc = zlicz(znaki);
+    ilosc = strlen(znaki);
+    //cout << ilosc;
+    ascii(znaki, ilosc);
+    odwroc(znaki);
 	return 0;
 }
 
