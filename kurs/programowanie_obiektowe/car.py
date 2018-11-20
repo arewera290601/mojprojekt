8c7f1328b484959aad7e21feb5f1804f37c8aec5
arewera290601 <aleksandrarewera@gmail.com>
2018-11-20 16:45:03 +0100

    kurs

--- kurs/programowanie_obiektowe/car.py
+++ kurs/programowanie_obiektowe/car.py
@@ -2,26 +2,30 @@
 # -*- coding: utf-8 -*-
 #
 #  car.py
-# napisz definicję obiektu samochód z nast. atrybutami:
+#
+# napisz definicję obiektu samochód z następującymi atrybutami:
 # marka, np. "Fiat"
 # model, np. "Tipo"
-#  rok produkcji, np. "2005"
+# rok produkcji, np. "2005"
 # nadwozie, np. "sedan"
 # metody obiektu:
-# wiek() - zwraca wiek w latach
+# wiek() - zwraca wiek auta w latach
+ 
 from datetime import date
-
-class samochod():
+ 
+class Car():
     
-    def __init__(self, marka, model, rokp, nadwozie):
+    def __init__(self, marka, model, rocznik, nadwozie):
         self.marka = marka
         self.model = model
-        self.rokp = rokp
+        self.rocznik = rocznik
         self.nadwozie = nadwozie
-        
-    def wiek(self):
-        return date.today().year - self.rokp
-
-        
-samochod = samochod('Fiat', 'Tipo', '2005', 'Sedan')
-print("Wiek: ", s.wiek())
+ 
+    def wiek(self, rocznik):
+        self.dzis = date.today().year
+        self.wiek = self.dzis - self.rocznik
+        print(self.wiek)
+ 
+car1 = Car('BMW', 'M3', 2004, 'sedan')
+print(car1.marka, car1.model, car1.rocznik, car1.nadwozie)
+car1.wiek(car1)
