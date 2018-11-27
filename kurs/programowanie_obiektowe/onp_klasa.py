b1bae77b0e1aa6f0198095f0a930d36d9a244b16
arewera290601 <aleksandrarewera@gmail.com>
2018-11-27 16:40:03 +0100

    todo

--- kurs/programowanie_obiektowe/onp_klasa.py
+++ kurs/programowanie_obiektowe/onp_klasa.py
@@ -25,7 +25,7 @@ class ONPKlasa(Stos):
         return True
 
     def oblicz_onp(self):
-        onp = self.onp_str.split(" ")
+        onp = self.onp_str.strip().split(" ")
     
         for el in onp:
            if el.isdigit():
@@ -35,23 +35,23 @@ class ONPKlasa(Stos):
                b = self.pop()
                self.log.append(str(b) + el + str(a))
                self.push(eval(str(b) + el + str(a)))
-               
-        self.wynik = self.pop()
         
+        self.wynik = self.pop()
+
     def p(self, operator):
         if operator == '(':
-            return 0 
+            return 0
         if operator == '+' or operator == '-':
             return 1
         if operator == '*' or operator == '/':
             return 2
         if operator == '**':
             return 3
-    
+
     def a2onp(self):
         operatory = set(['+', '-', '*', '/', '**'])
         for znak in self.a_str:
-            print("badany znak", znak)
+            print("Badany znak:", znak)
             if znak == " ":
                 continue
             elif znak == '(':
@@ -59,7 +59,7 @@ class ONPKlasa(Stos):
             elif znak == ')':
                 while self.peek() != '(':
                     self.onp_str += self.pop() + ' '
-                self.pop() # usunięcie nawiasu otwierjącego ze stosu
+                self.pop()  # usunięcie nawaisu otwierającego ze stosu
             elif znak in operatory:
                 while not self.isEmpty():
                     if self.p(znak) == 3 or self.p(znak) > self.p(self.peek()):
@@ -67,23 +67,25 @@ class ONPKlasa(Stos):
                     self.onp_str += self.pop()
                 self.push(znak)
             else:
-                self.onp_str += znak + ' ' 
+                self.onp_str += znak + ' '
         while self.peek():
             self.onp_str += self.pop() + ' '
         self.onp_str.strip()
-    
+
 def main(args):
-    # onp = input("Podaj wyr. ONP, oddzielając operandy i operatory spacjami:\n")
-    # o1 = ONPKlasa(onp)
-    # o1.oblicz_onp()
+    #onp = input("Podaj wyr. ONP, oddzielając operandy i operatory spacjami:\n")
+    #o1 = ONPKlasa(onp)
+    #o1.oblicz_onp()
     
-    print("Obliczenia: ", o1.log)
-    print("Wynik: ", o1.wynik)
-    o2 = ONPKlasa(a_str='4 + 5')
+    o2 = ONPKlasa(a_str='(4 + 5) * 6')
     o2.a2onp()
     print(o2.onp_str)
+    o2.oblicz_onp()
+    print("Obliczenia: ", o2.log)
+    print("Wynik: ", o2.wynik)
+    
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
