ecb612232f32fbabccfccc3be73e2a0478100ffe
arewera290601 <aleksandrarewera@gmail.com>
2018-11-30 14:20:40 +0100

    slonce

---
+++ ALGORYTMY/slonce.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  slonce.py
+#  
+from turtle import * 
+color('pink', 'black')
+begin_fill() # zacznij wypełniać
+for i in range(4):
+    forward(200)
+    right(90)
+done()
+
--- cpp/horner.cpp
+++ cpp/horner.cpp
@@ -9,14 +9,27 @@
 #include <iostream>
 using namespace std;
 
-void drukujw(int st, float tb[]){
+void drukujw(int st, float tb[]) {
     // 2x^3 + 3x^2 + 5x + 4 
-    int i = 0;
-    for(i = 0; i < st; i++) {
-        cout << tb[i] << "x^" << st - i << " + ";
-        }
-    cout << tb[i];
+    int i;
+    for(i = 0; i < st; i++) 
+        cout << tb[i] << "x^" << st-i << " + ";
+    cout << tb[i] << endl;
+        
     }
+
+float horner_it(float x, int stopien, float tbwsp[]){
+    // x(x(2x+ 3) + 5) + 4
+    float wynik;
+    wynik = tbwsp[0];
+    
+    for(int i = 1; i < stopien + 1; i++)
+        wynik = wynik * x + tbwsp[i];
+    
+    
+    
+    return wynik;
+     }
 
 
 int main(int argc, char **argv)
@@ -28,7 +41,7 @@ int main(int argc, char **argv)
     cin >> stopien;
     
     tbwsp = new float [stopien + 1];
-    cout << tbwsp;
+    //cout << tbwsp;
     
     for(int i=0; i <= stopien; i++){
         cout << "Podaj współczynnik przy najwyższej potędze " << stopien-i << ": ";
@@ -40,7 +53,7 @@ int main(int argc, char **argv)
     
     cout << "Wartość wielomianu o postaci: ";
     drukujw(stopien, tbwsp);
-    
+    cout << "wynosi: " << horner_it(x, stopien, tbwsp);
     
 	return 0;
 }
