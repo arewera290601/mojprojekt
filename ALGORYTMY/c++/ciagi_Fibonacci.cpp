48766e0f28a8253f85c856688996a2a937a5d9e6
arewera290601 <aleksandrarewera@gmail.com>
2018-12-07 14:19:09 +0100

    rekurencje

--- ALGORYTMY/c++/ciagi_Fibonacci.cpp
+++ ALGORYTMY/c++/ciagi_Fibonacci.cpp
@@ -5,9 +5,19 @@
 
 
 #include <iostream>
-
 using namespace std;
 
+// f(n) = f(n-1) + f(n-2)
+// f(5)
+
+
+long int fibonacci_re(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    return fibonacci_re(n-1) + fibonacci_re(n-2);
+    }
+    
+    
 long int fibonacci_it(int n){
     long int a = 0; // wyraz n -2
     long int b = 1; // wyrz n -1
@@ -31,6 +41,9 @@ int main(int argc, char **argv)
     cout << "Ciąg Fibonacciego do wyrazu " << n << ":" << endl;
     for (int i = 0; i <= n; i++){
         cout << fibonacci_it(i) << endl;
+       // cout << fibonacci_re(i) << endl;
+        if (i > 2)
+            cout << (float)fibonacci_it(i) / (float)fibonacci_it(i-1) << endl;
     }
 	return 0;
 }
