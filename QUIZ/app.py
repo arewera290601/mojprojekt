4b0cbc90718cbaf0eb617a133329abcd9b23372d
arewera290601 <aleksandrarewera@gmail.com>
2018-12-17 08:45:10 +0100

    aplikacja

--- QUIZ/app.py
+++ QUIZ/app.py
@@ -6,21 +6,21 @@
 # / - symbolizuje główny katalog serwera
 # 
 #  app.py
-#
 
-from flask import Flask
-from flask import render_template
+from flask import g
+from modele import *
+from views import *
 
-app = Flask(__name__)
+@app.before_request
+def before_request():
+    g.db = baza
+    g.db.connect()
 
-# widok domyślny
-@app.route("/")
-def hello():
-    return render_template('index.html')
-
-@app.route("/strona")
-def strona():
-    return render_template('strona.html')
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- QUIZ/baza.py
+++ QUIZ/baza.py
@@ -3,45 +3,54 @@
 #
 #  baza.py
 #  
-import os.path
+import os
 import csv
+from modele import *
 
 def czy_jest(plik):
-    """
-    funkcja sprawdza istnienie pliku na dysku
-    """
-    if not os.path.isfile(plik): #jeżeli pliku nie ma na dysku
+    """ Funkcja sprawdza istnienie pliku na dysku """
+    if not os.path.isfile(plik):
         print("Plik {} nie istnieje!".format(plik))
         return False
     return True
-    
+
+
 def dane_z_pliku(nazwa_pliku, separator=','):
     dane = []  # pusta lista na dane
     if not czy_jest(nazwa_pliku):
         return dane
+    
     with open(nazwa_pliku, 'r', newline='', encoding='utf-8') as plik:
         tresc = csv.reader(plik, delimiter=separator)
         for rekord in tresc:
             rekord = [x.strip() for x in rekord]  # oczyszczamy dane
-            dane.append(rekord)  # dodawanie rekordów do listy
-    return dan  
-
+            dane.append(rekord)  # dodawanie rekordĂłw do listy
+    return dane
 
 def dodaj_dane(dane):
-    dane = {
-        Pytanie: 'pytania',
-        Odpowiedz: 'odpowiedzi'
-    
-    }
     for model, plik in dane.items():
         pola = [pole for pole in model._meta.fields]
         pola.pop(0)
-        wpisy = dane_z_pliku(plik + '.csv')
+        wpisy = dane_z_pliku(plik + '.csv', ';')
+        print(wpisy)
         with baza.atomic():
             model.insert_many(wpisy, fields=pola).execute()
 
-
 def main(args):
+    if os.path.exists(baza_plik):
+        os.remove(baza_plik)
+    baza.connect()
+    baza.create_tables([Kategoria, Pytanie, Odpowiedz])
+    
+    dane = {
+        Kategoria: 'kategorie',
+        Pytanie: 'pytania',
+        Odpowiedz: 'odpowiedzi',
+    }
+
+    dodaj_dane(dane)
+    baza.commit()
+    baza.close()
     return 0
 
 if __name__ == '__main__':
