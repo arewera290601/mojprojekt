fad80f3beb9a5b0ea4a7a6c0b1a875d02d7576da
arewera290601 <aleksandrarewera@gmail.com>
2019-01-03 08:44:52 +0100

    cw

---
+++ ALGORYTMY/python/algorytm4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  algorytm4.py
+#  
+
+def znajdz()
+
+
+def main(args):
+     a = int(input('Podaj 1. liczbę: '))
+    print(a)
+    b = int(input('Podaj 2. liczbę: '))
+    print(b)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
