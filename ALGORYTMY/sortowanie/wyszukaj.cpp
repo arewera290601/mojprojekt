076db31564281c0900feef1d8c76e7fec341c28a
arewera290601 <aleksandrarewera@gmail.com>
2019-01-11 14:17:50 +0100

    sortowanie

--- ALGORYTMY/sortowanie/wyszukaj.cpp
+++ ALGORYTMY/sortowanie/wyszukaj.cpp
@@ -45,18 +45,31 @@ int szukaj_lin(int tab[], int n, int szuk) {
 // algorytm dziel i zwyciężaj- dzielić każda część na pół az zostanie jeden element
 
 int szukaj_bin_lin(int tab[], int n, int szuk){
-    int p, k, s;
-    p = 0; k = n - 1;
+    int p, k, s, w;
+    p = 0; k = n - 1; w = -1;
     while ( p <= k) {
         s = (p + k) / 2;
         if (tab[s] == szuk) {
-            p = s;
+            w = s;
             break;
         } else if (szuk < tab[s]) k = s - 1;
         else p = s + 1;
         }
-        return p;
+        return w;
     }
+    
+int szukaj_bin_rek(int tab[], int szuk, int p, int k){
+    if(p <= k){
+        int s = (p + k) /2;
+        if (tab[s] == szuk) return s;
+        if (szuk < tab[s]) 
+            return szukaj_bin_rek(tab, szuk, p, s-1);
+        else 
+            return szukaj_bin_rek(tab, szuk, s+1, k);
+        }
+    return -1;
+    }
+    
 int main(int argc, char **argv)
 {
 	int n = 20;
@@ -72,7 +85,7 @@ int main(int argc, char **argv)
         //~cout << "Nie znaleziono!";
     sort_insert(tab, n);
     int indeks = szukaj_bin_lin(tab, n, szuk);
-     
+    // int indeks = szukaj_bin_rek(tab, szuk, 0, n-1);
     if (indeks >= 0) cout << "Znaleziono: " << indeks << endl;
     else cout << "Nie znaleziono";
     return 0;
