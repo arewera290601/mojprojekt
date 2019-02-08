89a6129b80d2e5be997e4b7e1cfc7d5dceed4aaa
arewera290601 <aleksandrarewera@gmail.com>
2019-02-08 14:20:42 +0100

    fdgf

---
+++ Sieci_Komputerowe/cwiczenie.py
@@ -0,0 +1,16 @@
+The rain in Spain falls mainly on  the plain. (44 bajty)
+Zażółć gęślą jaźń.
+The rain <3,3>Sp<9,4>falls m<11,3>ly on t<34,3>pl<15,3>.(33 bajty)
+
+Rc = 33/44 * 100% = 75%
+
+<x, y> - token
+x =34 6 biyów
+32+16+8+4+2+1
+100010
+
+y = 4
+3bity
+100
+
+6+3= 9b =1B
