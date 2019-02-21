367162923e1ab8880f16ff71044c4a2593ef6ea4
arewera290601 <aleksandrarewera@gmail.com>
2019-02-21 19:51:27 +0100

    szyk przestawieniowy

--- Sieci_Komputerowe/cpp/szyfr_przestawieniowy.cpp
+++ Sieci_Komputerowe/cpp/szyfr_przestawieniowy.cpp
@@ -2,45 +2,32 @@
  * szyfr_przestawieniowy.cpp 
  */
 
+#include<iostream>
+#include<cstring>
 using namespace std;
 
-#include <iostream>
-#include <string.h>
-#define MAKS 100
-
-void deszyfruj(char tb[], int klucz){
-    klucz = klucz % 26;
-    int i = 0;
-    int kod = 0;
-    while (tb[i] != '\0'){
-        kod = (int)tb[i] - klucz;
-        if (tb[i] == ' '){
-            kod += klucz;
-        } else if (kod > 122){
-            kod += 26;
-        }
-        cout << (char)kod;
-        tb[i]= (char)kod;
-        i++;
-    }
-    cout << endl;
-    }
-
-void szyfruj(char tb[], int klucz){
-   int ile = strlen(tb);
-   ile znakow uzupelnic kropkami i uzupeklnic tekst tyki kropkami
+void kodowanie(char *napis)
+{
+	int dl = strlen(napis); //wyznaczenie liczby znaków
+	
+	for(int i=0; i<dl-1; i+=2) //przesuwamy się o dwa znaki
+	//zamiana sąsiadujących znaków
+	{
+		char pom = napis[i];
+		napis[i] = napis[i+1]; //dlatego w pętli i<dl-1
+		napis[i+1] = pom;	
+	}
 }
 
-int main(int argc, char **argv)
+int main()
 {
-	char tekst[MAKS];
-    int klucz = 0;
-    
-    cout << "Podaj tekst w małych literach: " << endl;
-    cin.getline(tekst, MAKS);
-    cout << "Podaj klucz: ";
-    cin >> klucz;
-    szyfruj(tekst, klucz);
-    deszyfruj(tekst, klucz);
+	char napis[100];
+	
+	cout<<"Podaj napis do zaszyfrowania: ";
+	cin.getline(napis, 100);
+	kodowanie(napis);
+	cout<<"Szyfrogram: ";
+	cout<<napis<<endl;
+	
 	return 0;
 }
