2f237e419497aa13648fc5297c70e094e6d73d7c
arewera290601 <aleksandrarewera@gmail.com>
2019-12-20 07:24:54 +0100

    frwg

---
+++ klasa 3/cpp/typydanych.cpp
@@ -0,0 +1,22 @@
+/*
+ * typydanych.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+// typ wyliczeniowy
+enum REZULTAT {
+    ISTNIEJE = 1, 
+    NIEISTNIEJE = -1,
+};
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
